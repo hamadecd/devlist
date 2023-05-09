4d6ca2b8df2bc57b8bcabbee999a38432ef390ac
Samir <samircd9@gmail.com>
2023-05-09 19:35:11 -0300

    Projeto criado

---
+++ src/test/java/com/devhamade/devlist/DevlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhamade.devlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
