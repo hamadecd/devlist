b2db457fe725e0fa14987f4d6d2955aa3802ab95
Samir <samircd9@gmail.com>
2023-05-11 22:41:24 -0300

    Deploy ready

--- src/main/java/com/devhamade/devlist/dto/GameMinDTO.java
+++ src/main/java/com/devhamade/devlist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devhamade.devlist.dto;
 
 import com.devhamade.devlist.entities.Game;
+import com.devhamade.devlist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -18,6 +19,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/devhamade/devlist/services/GameService.java
+++ src/main/java/com/devhamade/devlist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devhamade.devlist.services;
 import com.devhamade.devlist.dto.GameDTO;
 import com.devhamade.devlist.dto.GameMinDTO;
 import com.devhamade.devlist.entities.Game;
+import com.devhamade.devlist.projections.GameMinProjection;
 import com.devhamade.devlist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,12 @@ public class GameService {
     public GameDTO findById(Long id) {
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(game -> new GameMinDTO(game)).toList();
     }
 
 }
